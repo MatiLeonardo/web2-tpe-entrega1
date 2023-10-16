7a7cae27dc25052e791125303e4ca19ca95f83be
Gonzalo <gonzagiacomino18@gmail.com>
2023-10-16 15:02:52 -0300

    Faltaba un punto

--- init.php
+++ init.php
@@ -1,5 +1,5 @@
 <?php
 include_once 'config.php';
-include_once '/app/helpers/db.helper.php';
+include_once './app/helpers/db.helper.php';
 
 DatabaseHelper::crearDbSiNoExiste(DB_HOST, DB_USER, DB_PASS, DB_NAME);

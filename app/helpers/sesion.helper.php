17a4e0500a960849a75408e70b8151cf03dab438
Gonzalo <gonzagiacomino18@gmail.com>
2023-10-15 22:29:15 -0300

    Solucionados errores de autentifiación

--- app/helpers/sesion.helper.php
+++ app/helpers/sesion.helper.php
@@ -15,20 +15,26 @@ class SesionHelper
         SesionHelper::init();
         $_SESSION['USER_ID'] = $usuario->id;
         $_SESSION['USERNAME'] = $usuario->usuario;
-        $_SESSION['ISADMIN'] = $usuario->isAdmin;
+        $_SESSION['ISADMIN'] = $usuario->isAdmin; //ADMIN = 1
     }
 
     public static function verify()
     {
         SesionHelper::init();
-        if (!isset($_SESSION['USER_ID'])) {
-            header("Location: " . LOGIN);
+        if ($_SESSION['ISADMIN'] != 1) {
+            SesionHelper::logout();
+            header("Location: " . LOGIN); //SI SE VERIFICA QUE NO ES ADMIN(por ejemplo si el user sabe el path para eliminar algo), SE CIERRA SESION Y SE MANDA A LOGIN
             die();
         }
 
     }
 
-    public static function isAdmin()
+    public static function isLogged(){ // ESTA FUNCION SE UTILIZA PARA RETORNAR TRUE O FALSE Y MOSTRAR COSAS EN LA VISTA
+        SesionHelper::init();
+        return isset($_SESSION['USER_ID']);
+    }
+
+    public static function isAdmin() // ESTA FUNCION SE UTILIZA PARA RETORNAR TRUE O FALSE Y MOSTRAR COSAS EN LA VISTA
     {
         SesionHelper::init();
         if ( isset($_SESSION['ISADMIN']) && $_SESSION['ISADMIN'] == 1) {
